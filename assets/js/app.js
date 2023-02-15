1e919c6bb01e74f636c928d5fd87facac8a71876
JonyWhalee <jonathandr600@gmail.com>
2023-02-15 05:50:39 -0300

    firts commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+ direction: 'horizontal',
+ sliderPerView: 1,
+ spaceBetween: 0,
+ mousewheel: false,
+ pagination: {
+     el:'.swiper-pagination',
+     type: 'progressbar',
+ }
+}) 
+
